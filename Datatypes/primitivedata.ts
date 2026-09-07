1c9bad1f0eef09994f5fd33a80b95d347dced316
saipraveen9911 <saipraveen9911@gmail.com>
2026-09-07 22:51:55 +0530

    7-9-2026

---
+++ Datatypes/primitivedata.ts
@@ -0,0 +1,32 @@
+/* primitive data types
+
+number, string, boolen, any, union,null, undefined, void
+
+*/
+
+/* Non -primitive datatypes
+
+Arrays, class, function, object, tuple, interface
+
+*/
+//Number data type
+
+let int: number = 50;
+let float: number = 99.9;
+
+// String DataType
+// can be written in single-quote, double quote,
+
+let first_name: string = "sai";
+let middle_name: string = "praveen";
+let last_name: string = `hello ${first_name} ${middle_name} babu`;
+console.log(last_name);
+
+// boolean
+let isemployee: boolean = true;
+let lostjob: boolean = false;
+
+console.log(isemployee);
+console.log(lostjob);
+
+// any datatype
