d7af17d6236cb2f19216ef881becddf0ddf38711
saipraveen9911 <saipraveen9911@gmail.com>
2026-09-09 00:24:24 +0530

    8-9-2026 operators

---
+++ Operators.ts
@@ -0,0 +1,8 @@
+//arithmetic operator
+let a: number = 5;
+let b: number = 10;
+console.log(`addition of ${a} and ${b} is ${a + b}`); // addition
+console.log(`subtraction of ${a} and ${b} is ${a - b}`); // subtraction
+console.log(`multiplication of ${a} and ${b} is ${a * b}`); // multiplication
+console.log(`division of ${a} and ${b} is ${a / b}`); // division. returns quotient
+console.log(`modulus of ${a} and ${b} is ${a % b}`); // modulus returns remainder
