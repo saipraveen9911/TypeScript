8bc119360b032b35c34450b32e53b0316e5cee87
saipraveen9911 <saipraveen9911@gmail.com>
2026-09-12 22:53:38 +0530

    Arrays

--- Day_8_Arrays.ts
+++ Day_8_Arrays.ts
@@ -1,7 +1,19 @@
 // 11/09/2026
+/* Arrays: collection of Elements
 
-// collection of Elements
+*/
 
-let array: number[];
-let array1: Array<string>;
-let array2: Array<number | string | boolean>;
+//normal declaration of array
+let array: number[] = [];
+
+// declaration of array using generic datatype
+let array1: Array<string> = [];
+
+// declaring the array using Generic Array type with multiple datatype(union datatype)
+let array2: Array<number | string | boolean> = [];
+
+// adding th eelemets to array
+array[0] = 1122;
+array[1] = 30303;
+
+console.log(`the updated Array is [${array}]`);
